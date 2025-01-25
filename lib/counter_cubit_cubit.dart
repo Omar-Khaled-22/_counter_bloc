464d462c95538c118e9e34464883929fe541a194
OMAR\omark <omarkhaled2219@gmail.com>
2025-01-26 00:11:41 +0200

    -edited increment and decrement methods

--- lib/counter_cubit_cubit.dart
+++ lib/counter_cubit_cubit.dart
@@ -1,8 +1,10 @@
-import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 part 'counter_cubit_state.dart';
 
-class CounterCubitCubit extends Cubit<CounterCubitState> {
-  CounterCubitCubit() : super(CounterCubitInitial());
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
+
+  void increment() => emit(state + 1);
+  void decrement() => emit(state - 1);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -9,42 +9,34 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: BlocProvider(
-        create: (context) => CounterCubit(),
+        create: (_) => CounterCubit(),
         child: const MyHomePage(title: 'Flutter Demo Home Page'),
       ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: BlocConsumer<CounterCubit, int>(
         listener: (context, count) {
@@ -69,16 +61,14 @@ class _MyHomePageState extends State<MyHomePage> {
             );
           }
         },
-        builder: (context, state) {
+        builder: (context, count) {
           return Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                const Text(
-                  'You have pushed the button this many times:',
-                ),
+                const Text('You have pushed the button this many times:'),
                 Text(
-                  '$_counter',
+                  '$count',
                   style: Theme.of(context).textTheme.headlineMedium,
                 ),
               ],
@@ -91,19 +81,15 @@ class _MyHomePageState extends State<MyHomePage> {
         children: [
           FloatingActionButton(
             onPressed: () => context.read<CounterCubit>().increment(),
-            tooltip: 'Increment',
             child: const Icon(Icons.add),
           ),
           const SizedBox(height: 10),
           FloatingActionButton(
             onPressed: () => context.read<CounterCubit>().decrement(),
-            tooltip: 'Decrement',
             child: const Icon(Icons.remove),
           ),
         ],
       ),
-
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
