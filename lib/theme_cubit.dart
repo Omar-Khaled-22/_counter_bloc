d00eda1d50e51fcaa8c0cc8ff974c682b49d0a45
Omar-Khaled-22 <omarkhaledwr@gmail.com>
2025-01-26 00:42:51 +0200

    Update theme_cubit.dart

--- lib/theme_cubit.dart
+++ lib/theme_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-enum AppTheme { light, dark }
+
 
 class ThemeCubit extends Cubit<ThemeData> {
   ThemeCubit() : super(_lightTheme);
